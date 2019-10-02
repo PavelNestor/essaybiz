b58a56e4df247a5cccba605815c82ad7355c07ac
PavelNestor <pavelnesterets82@gmail.com>
2019-10-03 00:33:42 +0300

    work with tab-links

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,8 @@
 "use strict";
+
+const _$ = document.querySelector.bind(document);
+const _$$ = document.querySelectorAll.bind(document);
+
 import log from './test.js';
 
 
@@ -17,3 +21,29 @@ window.addEventListener('scroll', log('TEST'));
 //   menuImg.addEventListener("touch", onToogleMenu);
 //   menuClose.addEventListener("click", onToogleMenu);
 // })();
+
+// Topics TAB
+(function() {
+  const tabcontents = _$$('.tab__content');
+  const tablinks = _$$('.tab__links');
+
+  console.log('tabcontents', tabcontents);
+  console.log('tablinks', tablinks);
+
+  if (tabcontents.length < 1 || tablinks.length < 1) {
+    return;
+  };
+
+  const tabToggler = index => {
+    tabcontents.forEach(tabContent => tabContent.style.display = "none");
+    tablinks.forEach(tabLink => tabLink.classList.remove('active'));
+
+    tabcontents[index].style.display = "flex";
+    tablinks[index].classList.add('active');
+  };
+
+  tablinks.forEach((tabLink, index) => tabLink.addEventListener('click', () => tabToggler(index)));
+
+  tablinks[0].click();
+
+})();
