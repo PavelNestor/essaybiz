8c57ac57a044c9413b23ce7458530a4a823913b4
PavelNestor <pavelnesterets82@gmail.com>
2019-10-06 21:30:45 +0300

    refactor

--- src/js/menu-mobile.js
+++ src/js/menu-mobile.js
@@ -4,28 +4,18 @@ const $$ = document.querySelectorAll.bind(document);
 const menuOpen = $('#menu-open');
 const menuClose = $('#menu-close');
 const menuContent = $('#menu-mobile');
-// const body = $('body');
-
-// let scrollPosition = 0;
-
-// const calculateScrollPosition = () => {
-//   scrollPosition = document.body.getBoundingClientRect().top
-// };
-
-// window.addEventListener('scroll', calculateScrollPosition);
+const menuLinks = $$('.menu-mobile__link');
 
 (function() {
   const onOpenMenu = () => {
-    // body.style.top = `${- scrollPosition}px`;
-    // body.classList.add('fixedBody');
     menuContent.classList.add('menu-mobile_active');
   };
 
   const onCloseMenu = () => {
     menuContent.classList.remove('menu-mobile_active');
-    // body.classList.remove('fixedBody');
   };
 
+  menuLinks.forEach(link => link.addEventListener('click', () => menuContent.classList.remove('menu-mobile_active')));
   menuOpen.addEventListener('click', onOpenMenu);
   menuClose.addEventListener('click', onCloseMenu);
 })();
